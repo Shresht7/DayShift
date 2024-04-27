0d0f3335b4f0c1bcbe5ba7ae6a7ae796519bdc37
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-04-27 20:20:34 +0530

    Handle missing file gracefully

--- src/config.rs
+++ src/config.rs
@@ -26,7 +26,7 @@ impl Config {
     /// Read the configuration from the config file in the given path
     pub fn read(path: &str) -> Result<Config, Box<dyn std::error::Error>> {
         let path = std::path::Path::new(path).join("dayshift.config.json");
-        let contents = std::fs::read_to_string(path)?;
+        let contents = std::fs::read_to_string(path).unwrap_or(String::from("{}"));
         let config: Config = serde_json::from_str(&contents)?;
         Ok(config)
     }
