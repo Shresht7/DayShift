b113dd5bca935579fa8596b668844d9781235369
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-04-27 21:25:51 +0530

    Remove obsolete todo

--- src/commands.rs
+++ src/commands.rs
@@ -46,7 +46,7 @@ pub fn set(args: Vec<String>) {
     let wallpapers = helpers::get_wallpapers(path);
 
     // Divide the day into segments
-    let day = time::Day::new_with(config.start, config.end); // TODO: Allow custom day start and end times
+    let day = time::Day::new_with(config.start, config.end);
     let segments = day.divide(wallpapers.len() as u32);
 
     // Get current time of day
