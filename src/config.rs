279e6884d4943b68065b9a4450c948428854bb09
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-04-28 12:01:31 +0530

    Make the config file name a constant

--- src/config.rs
+++ src/config.rs
@@ -2,6 +2,9 @@
 use chrono::NaiveTime;
 use serde::Deserialize;
 
+/// Name of the configuration file
+const CONFIG_FILE: &str = "dayshift.config.json";
+
 /// Configuration for the application
 #[derive(Deserialize, Debug)]
 #[serde(default)]
@@ -33,7 +36,7 @@ impl Default for Config {
 impl Config {
     /// Read the configuration from the config file in the given path
     pub fn read(path: &str) -> Result<Config, Box<dyn std::error::Error>> {
-        let path = std::path::Path::new(path).join("dayshift.config.json");
+        let path = std::path::Path::new(path).join(CONFIG_FILE);
         let contents = std::fs::read_to_string(path).unwrap_or(String::from("{}"));
         let config: Config = serde_json::from_str(&contents)?;
         Ok(config)
